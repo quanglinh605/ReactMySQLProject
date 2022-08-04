2634894393b9ed244310e3ef64c69002c5358a38
quanglinh605 <quanglinh605@gmail.com>
2022-08-04 17:29:09 +0700

    update last day

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -14,11 +20,24 @@ connection.connect(function(err){
 });
 
 app.get('/api/news', (req, res) => {
-  var sql = "SELECT * FROM news";
+  var sql = "SELECT * FROM news ORDER BY id DESC";
   connection.query(sql, function(err, results) {
     if (err) throw err;
     res.json({news: results});
   });
 });
 
+app.post('/api/insert', function(req, res) {
+  var sql = "INSERT "
+          + "INTO news(title,description,content) "
+          + "VALUES('"
+          +   req.body.title+ "','" 
+          +   req.body.description + "','" 
+          +   req.body.content+"')";
+  connection.query(sql, function (err, results) {
+    if(err) throw err;
+    res.json({news: results});
+  });
+});
+ 
 app.listen(4000, () => console.log('App listening on port 4000'));
